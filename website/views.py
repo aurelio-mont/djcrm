85d28a621c8564a37355bba85eb67c5fc6d1883a
Aurelio Mont <aurelio.mont25@gmail.com>
2024-10-13 12:34:25 -0600

    ADD individual records page

--- website/views.py
+++ website/views.py
@@ -59,3 +59,15 @@ def register_user(request):
         return render(request, "register.html", {"form": form})
 
     return render(request, "register.html", {"form": form})
+
+
+def customer_record(request, pk):
+    """Customer record view"""
+
+    if request.user.is_authenticated:
+        # look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.error(request, "There was an error with your login, please try again")
+        return redirect("home")
